8d4e2e8fa78353f9e2fb1cb17b00aa9ba8224a8a
Gavin Turley <77turley@gmail.com>
2025-04-29 16:43:13 -0600

    W02 Abstraction Done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        
+        Resume resume = new Resume();
+        resume._name = "Gavin Turley";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
